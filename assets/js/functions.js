85afddb1fc891c94a9e124886920aeb2062f594d
Marco Antonio Augusto de Andrade <marco.andrade@ifro.edu.br>
2018-12-13 22:29:33 -0400

    Commit inicial

---
+++ assets/js/functions.js
@@ -0,0 +1,35 @@
+let md = document.querySelector('#md')
+let resultado = document.querySelector('#resultado');
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  calcular(md);
+});
+
+function calcular() {
+
+  resultado.className = 'mt-3 alert';
+
+  if (md.value < 60) {
+
+    let ef = Math.round((500 - (md.value * 6)) / 4);
+
+    if (ef <= 50){
+      resultado.classList.add('alert-warning');
+    } else if (ef > 50){
+      resultado.classList.add('alert-danger');
+    }
+
+    if (ef <= 100){
+      resultado.innerHTML = `Com ${md.value} de média na disciplina você precisa de <strong>${ef}</strong> pontos no exame final.`;
+    } else {
+      resultado.innerHTML = 'Nem com 100 pontos no exame final você será aprovado!';
+    }
+    
+  } else {
+    resultado.classList.add('alert-success');
+    resultado.innerHTML = `Com <strong>${md.value}</strong> pontos você está aprovado!`;
+  }
+  md.value = '';
+  md.focus();
+}
